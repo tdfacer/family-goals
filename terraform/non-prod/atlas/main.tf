19d2b02c38163e801e4159bdbfa74704ec93e891
Trevor Facer <trevorf@ifit.com>
2021-12-30 13:50:25 -0700

    Add descriptions to variables

--- terraform/non-prod/atlas/main.tf
+++ terraform/non-prod/atlas/main.tf
@@ -1,49 +1,68 @@
+# variables
+
+## provider
 variable "mongodbatlas_public_key" {
-  type = string
+  type        = string
+  description = "public key for mongodbatlas API access"
 }
 
 variable "mongodbatlas_private_key" {
-  type = string
+  type        = string
+  description = "private key for mongodbatlas API access"
 }
 
+## project
 variable "project_name" {
-  type = string
+  type        = string
+  description = "name of the atlas project"
 }
 
 variable "org_id" {
-  type = string
+  type        = string
+  description = "id of the atlas org"
 }
 
+## cluster
 variable "cluster_name" {
-  type = string
+  type        = string
+  description = "name of the atlas cluster"
 }
 
 variable "create_cluster" {
-  type = bool
+  # if using free tier, atlas API does not support create via API
+  type        = bool
+  description = "whether or not to create the atlas cluster"
 }
 
 variable "cluster_version" {
-  type = string
+  type        = string
+  description = "mongodb version, e.g. 4.4"
 }
 
+## db
 variable "database_name" {
-  type = string
+  type        = string
+  description = "name of the database"
 }
 
+## user
 variable "user_name" {
-  type = string
+  type        = string
+  description = "name of the user that will be used by the application"
 }
 
 variable "user_password" {
-  type = string
+  type        = string
+  description = "password of the user that will be used by the application"
 }
 
-# access
+## access
 variable "whitelist_ips" {
   type = map(object({
     ip          = string
     description = string
   }))
+  description = "object map of ips to whitelist"
 }
 
 terraform {
